22a88f52de3252c9a6ce51206c8ea02aae784afd
Magnus <magnus.oye@gmail.com>
2019-05-20 21:13:10 +0200

    Testing servos

--- src/app.py
+++ src/app.py
@@ -11,10 +11,23 @@ from pid import PID
 
 eyes = Eyes()
 servo = Servo()
-pidX = PID(1.0, 0.0, 0.0)
+
 
 running = True
+offsetY = 80
+offsetX = 90
+
+def translate(x, lowerIn, upperIn, lowerOut, upperOut):
+    """Map in value range to another range"""
+    y = (x - lowerIn) / (upperIn - lowerIn) * (upperOut - lowerOut) + lowerOut
+    return y
+
 
 if __name__ == "__main__":
     while(running):
         face_coordinates = eyes.detectFace()
+        x = translate(face_coordinates[0], 0, 640, 0, 180)
+        y = translate(face_coordinates[1], 0, 480, 50, 110)
+        servo.turnXAxis(x)
+        servo.turnYAxis(y)
+        
